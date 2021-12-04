22b908bdf1463a4946dc67853cfd1e02f65a5d77
tiktikimelbo7 <tiktikimelbo7@gmail.com>
2021-12-04 15:50:02 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -208,7 +208,7 @@ def bot_help(update, context):
 
 botcmds = [
         (f'{BotCommands.HelpCommand}','Get Detailed Help'),
-        (f'{BotCommands.StartCommand}','Start The Bot 😊'),
+        (f'{BotCommands.StartCommand}','Start The Bot'),
         (f'{BotCommands.MirrorCommand}','Start Mirroring'), 
         (f'{BotCommands.ZipMirrorCommand}','Start mirroring and upload as .zip'),
         (f'{BotCommands.UnzipMirrorCommand}','Extract files'),
